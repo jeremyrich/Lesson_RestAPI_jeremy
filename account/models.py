5907e637399eb3cd792624538b9b35edd997b50b
jeremyrich <jeremyrich@free.fr>
2019-03-08 16:06:02 +0100

    change account foreign key name

--- account/models.py
+++ account/models.py
@@ -28,7 +28,7 @@ class Students(models.Model):
     insert_date = models.DateTimeField(auto_now_add=True)
     update_date = models.DateTimeField(auto_now=True)
 
-    account = models.ForeignKey(Account, related_name='students', on_delete=models.CASCADE)
+    account_id = models.ForeignKey(Account, related_name='students', on_delete=models.CASCADE)
 
     class Meta:
         verbose_name_plural = "Students"
